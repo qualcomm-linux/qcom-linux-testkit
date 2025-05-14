084874529756ee72e4ae86cc26853364daf64914
Abhishek <abbajaj@qti.qualcomm.com>
2025-05-14 12:49:57 +0530

    Public CI fast RPC Shell Scripts
    Added shell script for fast RPC testcases
    Added Readme for fast RPC Testcases
    
    Addressed comments for earlier commit
    
    Signed-off-by: Abhishek <abbajaj@qti.qualcomm.com>

--- Runner/suites/Multimedia/CDSP/fastrpc_test/run.sh
+++ Runner/suites/Multimedia/CDSP/fastrpc_test/run.sh
@@ -3,7 +3,7 @@
 
 #!/bin/sh
 # Import test suite definitions
-/var/Runner/init_env
+. $(pwd)/init_env
 TESTNAME="fastrpc_test"
 
 #import test functions library
@@ -13,21 +13,12 @@ test_path=$(find_test_case_by_name "$TESTNAME")
 log_info "-----------------------------------------------------------------------------------------"
 log_info "-------------------Starting $TESTNAME Testcase----------------------------"
 
-
 log_info "Checking if dependency binary is available"
 export PATH=$PATH:/usr/share/bin
 check_dependencies fastrpc_test
 
 mkdir -p results/fastrpc_test
 chmod -R 755 results/fastrpc_test
-
-# Start logs
-dmesg -C
-tail -f /var/log/syslog > results/fastrpc_test/syslog_log.txt &
-SYSLOG_PID=$!
-dmesg -w > results/fastrpc_test/dmesg_log.txt &
-DMESG_PID=$!
-
 
 # Navigate to the directory where the fastrpc_test application is located
 
@@ -40,14 +31,10 @@ echo $output
 # Check if the output contains the desired string
 if echo "$output" | grep -q "All tests completed successfully"; then
 	log_pass "$TESTNAME : Test Passed"
-	echo "$TESTNAME : Test Passed" > $test_path/$TESTNAME.res
+    echo "$TESTNAME PASS" > $test_path/$TESTNAME.res
 else
 	log_fail "$TESTNAME : Test Failed"
-	echo "$TESTNAME : Test Failed" > $test_path/$TESTNAME.res
+	echo "$TESTNAME FAIL" > $test_path/$TESTNAME.res
 fi
-
-# Cleanup
-kill $DMESG_PID
-kill $SYSLOG_PID
 
 log_info "-------------------Completed $TESTNAME Testcase----------------------------"
