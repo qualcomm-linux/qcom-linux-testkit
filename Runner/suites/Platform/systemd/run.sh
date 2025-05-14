271642228d7e24027956aaea60b7872bcfbae578
Abhishek Sinha <abhsinh@qti.qualcomm.com>
2025-05-14 15:02:43 +0530

    moving shebang line to non bash specific
    Signed-off-by: Abhishek Sinha <abhsinh@qti.qualcomm.com>

--- Runner/suites/Platform/systemd/run.sh
+++ Runner/suites/Platform/systemd/run.sh
@@ -1,6 +1,6 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
-#!/bin/bash
+#!/bin/sh
 
 # Function to check if systemd is running with PID 1
 check_systemd_pid() {
