e6550cebe58c3af30870d613f300ec67748de4c4
anilyada <anilyada@qti.qualcomm.com>
2025-05-16 17:15:28 +0530

    license: Added SPDX BSD-3-Clause-Clear license headers to test scripts
    This commit adds SPDX license identifiers and copyright notices to all
    shell scripts included in the test suite. The license used is
    BSD-3-Clause-Clear, in accordance with Qualcomm Technologies' licensing
    policy.
    
    Each script now includes:
    - SPDX-License-Identifier: BSD-3-Clause-Clear
    - Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
    
    This ensures proper attribution and compliance with open source licensing
    standards.
    
    Signed-off-by: anilyada <anilyada@qti.qualcomm.com>

--- Runner/suites/Connectivity/Bluetooth/run.sh
+++ Runner/suites/Connectivity/Bluetooth/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+
 # Import test suite definitions
 . $(pwd)/init_env
 TESTNAME="Bluetooth"
--- Runner/suites/Kernel/FunctionalArea/baseport/shmbridge/run.sh
+++ Runner/suites/Kernel/FunctionalArea/baseport/shmbridge/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 
 # Import test suite definitions
 source "$(pwd)/init_env"
